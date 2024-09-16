4e9804d282a986ef69f7825ecae5a0e55ced54ec
Jianmin Zhu <quic_jianminz@quicinc.com>
2024-09-15 21:15:16 -0700

    qcacld-3/0: Don't do CAC when only puncture bitmap changed
    After SAP DFS puncture enabled, if radar happens on 1 channel, just
    puncture that channel, no channel change, after SAP CSA, still do CAC,
    SAP can't tx until 60 sec later, all connected ref peers disconnect,
    which is bad user experience.
    
    To address it, if no freq and channel width change, only puncture bitmap
    changed, don't reset CAC end notify, then don't need another CAC after
    CSA.
    
    Change-Id: I4a2c919645424714f78931a02e05ac733bae5db7
    CRs-Fixed: 3918157

--- core/sap/src/sap_fsm.c
+++ core/sap/src/sap_fsm.c
@@ -258,9 +258,12 @@ sap_is_chan_change_needed_for_radar(struct sap_context *sap_ctx,
 							 CH_WIDTH_20MHZ,
 							 &pri_freq_puncture);
 		if (QDF_IS_STATUS_SUCCESS(status) && !pri_freq_puncture) {
-			sap_debug("Eht valid puncture : 0x%x, keep freq %d",
+			sap_debug("Eht valid puncture : 0x%x, keep freq %d bw %d ccfs0 %d ccfs1 %d",
 				  ch_params->reg_punc_bitmap,
-				  sap_ctx->chan_freq);
+				  sap_ctx->chan_freq,
+				  ch_params->ch_width,
+				  ch_params->mhz_freq_seg0,
+				  ch_params->mhz_freq_seg1);
 			mac_ctx->sap.SapDfsInfo.new_chanWidth =
 						ch_params->ch_width;
 			*freq = sap_ctx->chan_freq;
@@ -584,7 +587,7 @@ bool sap_plus_sap_cac_skip(struct mac_context *mac,
 		struct sap_context *sap_context =
 			mac->sap.sapCtxList[intf].sap_context;
 
-		if (!sap_context || sap_context == sap_ctx)
+		if (!sap_context)
 			continue;
 		if (mac->sap.sapCtxList[intf].sapPersona != QDF_SAP_MODE &&
 		    mac->sap.sapCtxList[intf].sapPersona != QDF_P2P_GO_MODE)
@@ -592,7 +595,7 @@ bool sap_plus_sap_cac_skip(struct mac_context *mac,
 		if (sap_context->isCacEndNotified &&
 		    sap_context->chan_freq == chan_freq &&
 		    sap_operating_on_dfs(mac, sap_context)) {
-			sap_debug("SAP vid %d CAC can skip due to CAC completed on other SAP vid %d",
+			sap_debug("SAP vid %d CAC can skip due to CAC completed on SAP vid %d",
 				  sap_ctx->sessionId, sap_context->sessionId);
 			return true;
 		}
