8ededb6e3da274125773a1cc2509bdc56ee1e751
Chenming Huang <quic_chenhuan@quicinc.com>
2024-09-30 12:55:50 +0530

    qcacld-3.0: Add HE mode to sir_sme_phy_mode
    Expand sir_sme_phy_mode to support HE mode.
    
    Change-Id: Ice850e6f98c78c2b2090cb526112bc2f0260d276
    CRs-Fixed: 3945840

--- core/mac/inc/sir_api.h
+++ core/mac/inc/sir_api.h
@@ -1037,7 +1037,8 @@ struct oem_channel_info {
 enum sir_sme_phy_mode {
 	SIR_SME_PHY_MODE_LEGACY = 0,
 	SIR_SME_PHY_MODE_HT = 1,
-	SIR_SME_PHY_MODE_VHT = 2
+	SIR_SME_PHY_MODE_VHT = 2,
+	SIR_SME_PHY_MODE_HE = 3,
 };
 
 /* / Definition for Association indication from peer */
