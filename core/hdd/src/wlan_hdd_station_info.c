dd6f884e8c88646bdd9c4a9120655f957decce34
Sheenam Monga <quic_shemonga@quicinc.com>
2025-02-03 08:25:29 +0530

    qcacld-3.0: Prevent stats processing during link reconfiguration
    Currently, requesting stats uses the link ID, but if link
    reconfiguration is in progress, the link may be deleted,
    resulting in failures.
    
    To prevent issues, don't process stats requests while link
    reconfiguration is ongoing.
    
    Change-Id: Idd09f80ec07b5194dbdf9b1f56653204c54fb86c
    CRs-Fixed: 4083011

--- core/hdd/src/wlan_hdd_station_info.c
+++ core/hdd/src/wlan_hdd_station_info.c
@@ -2569,6 +2569,7 @@ static int hdd_get_station_info_ex(struct wlan_hdd_link_info *link_info)
 	int ret;
 	struct hdd_station_info *stainfo = NULL;
 	struct qdf_mac_addr *peer_mac_addr;
+	struct wlan_objmgr_vdev *vdev;
 
 	hdd_sta_ctx = WLAN_HDD_GET_STATION_CTX_PTR(link_info);
 	ucfg_mc_cp_get_big_data_fw_support(hdd_ctx->psoc, &big_data_fw_support);
@@ -2576,11 +2577,29 @@ static int hdd_get_station_info_ex(struct wlan_hdd_link_info *link_info)
 	if (hdd_cm_is_disconnected(link_info) && big_data_fw_support)
 		big_data_stats_req = true;
 
+
+	vdev = hdd_objmgr_get_vdev_by_user(link_info, WLAN_OSIF_STATS_ID);
+	if (!vdev) {
+		hdd_err("vdev is NULL");
+		return -EINVAL;
+	}
+
+	/*
+	 * Don't process stats request if link reconfig is
+	 * in progress, as link can be deleted or link switch
+	 * may happen.
+	 */
+	if (mlo_is_link_recfg_in_progress(vdev)) {
+		hdd_debug("link reconfig already in progress");
+		hdd_objmgr_put_vdev_by_user(vdev, WLAN_OSIF_ID);
+		return -EBUSY;
+	}
+	hdd_objmgr_put_vdev_by_user(vdev, WLAN_OSIF_STATS_ID);
+
 	if (wlan_hdd_get_station_stats(link_info))
 		hdd_err_rl("wlan_hdd_get_station_stats fail");
 
 	wlan_hdd_get_peer_rx_rate_stats(link_info);
-
 	if (hdd_cm_is_vdev_connected(link_info)) {
 		peer_mac_addr = &link_info->session.station.conn_info.bssid;
 		stainfo = qdf_mem_malloc(sizeof(*stainfo));
