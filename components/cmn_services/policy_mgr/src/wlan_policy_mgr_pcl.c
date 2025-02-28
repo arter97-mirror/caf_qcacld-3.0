8e71a508049e964f9d414fecd85677b2856eaad1
Neetendra Singh <quic_neesing@quicinc.com>
2025-02-28 15:33:03 +0530

    qcacld-3.0: SAP Restart on Unsafe Channel in 5 GHz Band
    SAP is attempting to restart on the unsafe channel 5745, despite the
    entire 5 GHz band being marked as unsafe. Since 5745 is a NAN social
    channel, it is being removed from the unsafe list and marked as safe.
    This applies only if NAN discovery is enabled.
    
    Verify if NAN discovery is disabled, and if it is, do not mark 5745 as a
    safe channel.
    
    CRs-Fixed: 4071752
    Change-Id: I274eaee88d182760d201dc72d885be2051066682

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
@@ -656,7 +656,8 @@ void policy_mgr_update_with_safe_channel_list(struct wlan_objmgr_psoc *psoc,
 	nan_2g_freq =
 		policy_mgr_mode_specific_get_channel(pm_ctx->psoc,
 						     PM_NAN_DISC_MODE);
-	nan_5g_freq = wlan_nan_get_5ghz_social_ch_freq(pm_ctx->pdev);
+	if (wlan_nan_is_disc_active(psoc))
+		nan_5g_freq = wlan_nan_get_5ghz_social_ch_freq(pm_ctx->pdev);
 
 	for (i = 0; i < current_channel_count; i++) {
 		is_unsafe = 0;
