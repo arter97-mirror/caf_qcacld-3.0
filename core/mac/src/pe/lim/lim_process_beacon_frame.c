8ba2fc97613d16e61c370e296d74a32de7a3980c
Surya Prakash Sivaraj <quic_suryapra@quicinc.com>
2025-03-13 16:57:23 +0530

    qcacld-3.0: Process EHT OP irrespective of CU
    Host driver does not process the EHT OP IE changes on the
    home channel of assoc vdev, unless the BPCC/CU is set in
    the MLO IE. But, the spec mentions the following: "The BSS
    Parameters Change Count subfield is an unsigned integer,
    initialized to 0, that increments when a critical update to
    the BSS Parameters of the reported AP occurs."
    
    BPCC need not be set for changes to the "reporting" AP. It
    needs to be added only for the changes to the "reported" AP.
    
    To fix this, process the EHT OP IE change irrespective of the
    BPCC presence.
    
    Change-Id: I529cdb544e5c0bb42546646faccc95e080a10151
    CRs-Fixed: 4085816

--- core/mac/src/pe/lim/lim_process_beacon_frame.c
+++ core/mac/src/pe/lim/lim_process_beacon_frame.c
@@ -490,9 +490,6 @@ void lim_process_beacon_eht(struct mac_context *mac_ctx,
 	if (!des_chan || !IS_WLAN_PHYMODE_EHT(des_chan->ch_phymode))
 		return;
 
-	if (wlan_cm_is_vdev_connected(vdev))
-		lim_process_beacon_eht_op(session, bcn_ptr);
-
 	if (mlo_is_mld_sta(vdev))
 		/* handle beacon IE for 802.11be mlo case */
 		lim_process_beacon_mlo(mac_ctx, session, bcn_ptr);
@@ -662,6 +659,9 @@ lim_process_beacon_frame(struct mac_context *mac_ctx, uint8_t *rx_pkt_info,
 		lim_check_and_announce_join_success(mac_ctx, bcn_ptr,
 				mac_hdr, session);
 	}
+
+	if (wlan_cm_is_vdev_connected(session->vdev))
+		lim_process_beacon_eht_op(session, bcn_ptr);
 
 	if (cu_flag) {
 		lim_process_beacon_eht(mac_ctx, session, bcn_ptr);
