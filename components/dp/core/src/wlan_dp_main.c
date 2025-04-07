2760fe38c92f30ff74de16d37ce77651477da0bd
AJAY SINGH DAHIYA <asinghda@qti.qualcomm.com>
2025-04-07 17:42:22 +0530

    qcacld-3.0: Added HAPS feature to avoid unwanted FW wakeups
    Host Aware Power Save (HAPS) is a feature that, when enables,
    allows the firmware to update the host on its power states.
    The host then checks these states before deciding whether to
    coalesce packets. This process helps prevent unnecessary wakeups
    and also starts QTimer synchronization if enabled.
    
    Change-Id: Id6c42abbc6ab33ba4098f866119294f6ad818148
    CRs-Fixed: 4184956

--- components/dp/core/src/wlan_dp_main.c
+++ components/dp/core/src/wlan_dp_main.c
@@ -43,6 +43,7 @@
 #include "dp_internal.h"
 #endif
 #include "wlan_dp_svc.h"
+#include "wlan_dp_haps.h"
 
 #ifdef WLAN_DP_PROFILE_SUPPORT
 /* Memory profile table based on supported caps */
@@ -523,6 +524,26 @@ dp_nud_tracking_cfg_update(struct wlan_dp_psoc_cfg *config,
 }
 #endif
 
+#ifdef WLAN_HAPS_ENABLE
+/**
+ * dp_haps_cfg_update() - initialize HAPS config
+ * @config : Configuration parameters
+ * @psoc: psoc handle
+ */
+static void
+dp_haps_cfg_update(struct wlan_dp_psoc_cfg *config,
+		   struct wlan_objmgr_psoc *psoc)
+{
+	config->haps_config = cfg_get(psoc, CFG_HAPS_CONFIG);
+}
+#else
+static void
+dp_haps_cfg_update(struct wlan_dp_psoc_cfg *config,
+		   struct wlan_objmgr_psoc *psoc)
+{
+}
+#endif
+
 #ifdef QCA_SUPPORT_TXRX_DRIVER_TCP_DEL_ACK
 /**
  * dp_ini_tcp_del_ack_settings() - initialize TCP delack config
@@ -675,6 +696,7 @@ static void dp_cfg_init(struct wlan_dp_psoc_context *ctx)
 	dp_trace_cfg_update(config, psoc);
 	dp_nud_tracking_cfg_update(config, psoc);
 	dp_trace_cfg_update(config, psoc);
+	dp_haps_cfg_update(config, psoc);
 }
 
 /**
@@ -975,6 +997,8 @@ dp_vdev_obj_create_notification(struct wlan_objmgr_vdev *vdev, void *arg)
 	dp_nud_ignore_tracking(dp_intf, false);
 	dp_mic_enable_work(dp_intf);
 	dp_flow_priortization_init(dp_intf);
+	dp_vdev_haps_attach(wlan_psoc_get_dp_handle(psoc), dp_intf,
+			    vdev->vdev_objmgr.vdev_id);
 
 	return status;
 }
@@ -994,6 +1018,7 @@ dp_vdev_obj_destroy_notification(struct wlan_objmgr_vdev *vdev, void *arg)
 		return QDF_STATUS_E_INVAL;
 	}
 
+	dp_vdev_haps_detach(dp_intf);
 	dp_flow_priortization_deinit(dp_intf);
 	dp_nud_ignore_tracking(dp_intf, true);
 	dp_nud_reset_tracking(dp_intf);
