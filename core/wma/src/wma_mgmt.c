03b3b695010eb36443e15b4d1006f13c51588f23
Srinivas Dasari <dasaris@qti.qualcomm.com>
2025-04-30 20:54:17 -0700

    qcacld-3.0: Strip MIC from protected frames based on the key length
    Currently, 8-byte MIC is stripped from tail of the frame for
    NAN PASN-128 frames. But this MIC length can be of 16-bytes when
    encryption is GCMP, i.e. PASN-256.
    So, strip the MIC of right length by checking key_len for that
    corresponding peer. Strip 16-bytes of MIC when key_len is 32 bytes
    and 8-bytes of MIC when key_len is 16 bytes.
    
    Change-Id: Id660786c1748c45b7233239998dccd0248f9ccc7
    CRs-Fixed: 4135659

--- core/wma/src/wma_mgmt.c
+++ core/wma/src/wma_mgmt.c
@@ -3850,6 +3850,7 @@ int wma_process_rmf_frame(tp_wma_handle wma_handle,
 	uint8_t *ccmp;
 	uint8_t mic_len, hdr_len, pdev_id;
 	QDF_STATUS status;
+	struct wlan_crypto_key *key;
 
 	if ((wh)->i_fc[1] & IEEE80211_FC1_WEP) {
 		if (QDF_IS_ADDR_BROADCAST(wh->i_addr1) ||
@@ -3861,8 +3862,13 @@ int wma_process_rmf_frame(tp_wma_handle wma_handle,
 
 		if (iface->type == WMI_VDEV_TYPE_NDI ||
 		    iface->type == WMI_VDEV_TYPE_NAN) {
+			key = wlan_crypto_get_key(iface->vdev, wh->i_addr2, 0);
 			hdr_len = IEEE80211_CCMP_HEADERLEN;
-			mic_len = IEEE80211_CCMP_MICLEN;
+			if (key &&
+			    (key->keylen == WLAN_CRYPTO_KEY_GCMP_256_LEN))
+				mic_len = WLAN_IEEE80211_GCMP_MICLEN;
+			else
+				mic_len = IEEE80211_CCMP_MICLEN;
 		} else {
 			pdev_id =
 				wlan_objmgr_pdev_get_pdev_id(wma_handle->pdev);
