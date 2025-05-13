e1df4d748d2b0c1fbae4805f2e1ea9b45e7d0267
Naga Maneendra Pokuri <npokuri@qti.qualcomm.com>
2025-05-13 14:33:57 +0530

    qcacld-3.0: Limit regulatory connectivity IE to HE sessions
    Add regulatory connectivity IE only if the session is HE capable
    to avoid unnecessary IE population for non-HE clients and
    Extend pe_debug() to log presence of key IEs (RSNX, MSCS, MBO,
    vendor, adaptive 11r, WFA Gen Cap, MLO, FILS HLP).
    
    Change-Id: Ib32ad31447c960d037736d4336c23a352ad9e113
    CRs-Fixed: 4142874

--- core/mac/src/pe/lim/lim_send_management_frames.c
+++ core/mac/src/pe/lim/lim_send_management_frames.c
@@ -327,7 +327,8 @@ QDF_STATUS lim_send_probe_req_mgmt_frame(struct mac_context *mac_ctx,
 	}
 
 	/* Populate Non-AP STA Regulatory connectivity element */
-	populate_dot11f_reg_connectivity(mac_ctx, &pr->reg_connect);
+	if (IS_DOT11_MODE_HE(dot11mode))
+		populate_dot11f_reg_connectivity(mac_ctx, &pr->reg_connect);
 
 	if (addn_ielen && additional_ie) {
 		qdf_mem_zero((uint8_t *)&extracted_ext_cap,
@@ -3506,7 +3507,8 @@ lim_send_assoc_req_mgmt_frame(struct mac_context *mac_ctx,
 	}
 
 	/* Populate Non-AP STA Regulatory connectivity element */
-	populate_dot11f_reg_connectivity(mac_ctx, &frm->reg_connect);
+	if (IS_DOT11_MODE_HE(pe_session->dot11mode))
+		populate_dot11f_reg_connectivity(mac_ctx, &frm->reg_connect);
 
 	if (pe_session->is11Rconnection) {
 		struct bss_description *bssdescr;
@@ -3996,12 +3998,18 @@ lim_send_assoc_req_mgmt_frame(struct mac_context *mac_ctx,
 	MTRACE(qdf_trace(QDF_MODULE_ID_PE, TRACE_CODE_TX_MGMT,
 			 pe_session->peSessionId, mac_hdr->fc.subType));
 
-	pe_debug("extr_ext_flag %d mbo ie len %d is open auth %d stripped vendor len %d he with tkip %d ht %d vht %d opmode %d vendor vht %d he %d eht %d",
-		 extr_ext_flag, mbo_ie_len, is_open_auth, current_len,
-		 pe_session->he_with_wep_tkip,
-		 frm->HTCaps.present, frm->VHTCaps.present,
-		 frm->OperatingMode.present, frm->vendor_vht_ie.present,
-		 frm->he_cap.present, frm->eht_cap.present);
+	pe_debug("Assoc Req IEs: dot11mode %d, extcap %d, open %d, IE len:: mbo %d vendor %d, rsnx %d, mscs %d, rsn_sel %d, ft11r %d, wfa %d, mlo %d, eht %d, fils %d",
+		 pe_session->dot11mode, extr_ext_flag,
+		 is_open_auth, mbo_ie_len,
+		 vendor_ie_len, rsnx_ie_len,
+		 mscs_ext_ie_len,
+		 rsn_sel_ie_len,
+		 adaptive_11r_ie_len,
+		 wfa_gen_cap_ie_len,
+		 mlo_ie_len,
+		 eht_cap_ie_len,
+		 fils_hlp_ie_len);
+
 	pe_nofl_info("Assoc req TX: vdev %d to "QDF_MAC_ADDR_FMT" seq num %d",
 		     pe_session->vdev_id, QDF_MAC_ADDR_REF(pe_session->bssId),
 		     mac_ctx->mgmtSeqNum);
