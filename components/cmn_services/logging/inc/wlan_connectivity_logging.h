4fe5f653af2bd895049d948c2cbc9b11a5b0c612
Naga Maneendra Pokuri <npokuri@qti.qualcomm.com>
2025-05-19 15:26:31 +0530

    qcacld-3.0: Align diag_mlo_link_status struct with userspace format
    Fix structure mismatch between driver and userspace
    that was causing incorrect data parsing.
    update the driver struct to match userspace
    layout for proper parsing of the link status.
    
    Change-Id: I428a577832a74b0f36b4f7a3f12f1ef9cb75d1d9
    CRs-Fixed: 4134937

--- components/cmn_services/logging/inc/wlan_connectivity_logging.h
+++ components/cmn_services/logging/inc/wlan_connectivity_logging.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -770,12 +770,12 @@ struct wlan_diag_mlo_t2lm_teardown {
  */
 struct wlan_diag_mlo_link_status {
 	struct wlan_connectivity_log_diag_cmn diag_cmn;
-	uint32_t version;
-	uint32_t active_link:5;
-	uint32_t prev_active_link:5;
-	uint32_t associated_links:5;
-	uint32_t reserved:17;
-	uint32_t reason;
+	uint8_t version;
+	uint16_t active_link:5;
+	uint16_t prev_active_link:5;
+	uint16_t associated_links:5;
+	uint16_t reserved:1;
+	uint8_t reason;
 } qdf_packed;
 
 #define DIAG_NBR_RPT_VERSION 1
