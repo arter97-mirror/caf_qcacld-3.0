0edf69f9425c5b78b15cb2297193bd48a54b1f81
Srinivas Dasari <dasaris@qti.qualcomm.com>
2025-05-28 11:57:04 +0530

    qcacld-3.0: Derive current bw from StaDS instead of pe_session
    Current operating bandwidth is fetched from pe_session whereas
    it's supposed to be derived from StaDS. This is introduced as part
    of the commit Ib8c5dece6404492f361892adfb512a59c2f252da.
    Bring back the original logic to determine the current operating
    bw from StaDS to update to the operating bw of AP.
    Also, don't treat 80 MHz+80 MHz connection as 160 MHz.
    
    Change-Id: I4dcb8f3ebf09b27b610aa99bb6fb86ddf030cbd7
    CRs-Fixed: 4155495

--- core/mac/src/pe/lim/lim_utils.c
+++ core/mac/src/pe/lim/lim_utils.c
@@ -11704,7 +11704,19 @@ bool lim_update_channel_width(struct mac_context *mac_ctx,
 	if (cb_mode == WNI_CFG_CHANNEL_BONDING_MODE_DISABLE)
 		return false;
 
-	oper_mode = session->ch_width;
+	/* Don't treat 80+80 as 160MHz */
+	if (sta_ptr->htSupportedChannelWidthSet) {
+		if (sta_ptr->vhtSupportedChannelWidthSet >
+		    WNI_CFG_VHT_CHANNEL_WIDTH_80MHZ &&
+		    sta_ptr->vhtSupportedChannelWidthSet !=
+		    WNI_CFG_VHT_CHANNEL_WIDTH_80_PLUS_80MHZ)
+			oper_mode = CH_WIDTH_160MHZ;
+		else
+			oper_mode = sta_ptr->vhtSupportedChannelWidthSet + 1;
+	} else {
+		oper_mode = CH_WIDTH_20MHZ;
+	}
+
 	fw_vht_ch_wd = wlan_mlme_get_max_bw();
 
 	if (ch_width > fw_vht_ch_wd) {
