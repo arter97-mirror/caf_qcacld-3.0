6748b4051a75d5067bedffc3109a3a167fd7e0a8
Ming Jiang <quic_mjiang@quicinc.com>
2025-06-09 11:09:46 +0800

    qcacld30: Mark CLD version as PostCS-8
    Signed-off-by: Ming Jiang <quic_mjiang@quicinc.com>
    Change-Id: I8d9f7d3b20fd81d4668e9144de1f122a4c187535
    CRs-Fixed: 4175399

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            0
-#define QWLAN_VERSION_EXTRA            "PostCS-6"
+#define QWLAN_VERSION_EXTRA            "PostCS-8"
 #define QWLAN_VERSION_BUILD            252
 
-#define QWLAN_VERSIONSTR               "5.2.0.252-PostCS-6"
+#define QWLAN_VERSIONSTR               "5.2.0.252-"QWLAN_VERSION_EXTRA
 
 #endif /* QWLAN_VERSION_H */
