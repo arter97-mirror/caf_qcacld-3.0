fcef2fc333af8b0db927620580b6cf6a6c308fe5
Kiran Kumar Reddy A E <quic_kreddyae@quicinc.com>
2025-06-10 20:38:52 -0700

    Release 5.2.1.133W
    Release 5.2.1.133W
    
    Change-Id: I6d52e432ee5549aaf09e7be855b6b2c35aa1897e

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "V"
+#define QWLAN_VERSION_EXTRA            "W"
 #define QWLAN_VERSION_BUILD            133
 
-#define QWLAN_VERSIONSTR               "5.2.1.133V"
+#define QWLAN_VERSIONSTR               "5.2.1.133W"
 
 #endif /* QWLAN_VERSION_H */
