efa5bd2c6f2dc4770db30077fc18e06d0d931f07
Jyoti Kumari <jyotkuma@qti.qualcomm.com>
2025-06-16 22:58:37 +0530

    qcacld-3.0: Extract beacon RSSI history info from firmware
    As per requirement, host will query get_station command to
    firmware with stats id WMI_HOST_REQUEST_VDEV_RECV_BCN_STAT after
    every 1 sec and firmware will respond with raw beacon rssi and
    beacon tsf as part of WMI_UPDATE_STATS_EVENTID.
    For legacy sta, firmware will send these information for the
    requested vdev and for ml sta, it will send for both active and
    inactive vdevs.
    While receiving the event, host will extract raw beacon rssi and
    beacon tsf for stats id WMI_HOST_REQUEST_VDEV_RECV_BCN_STAT
    and send it to upper layer.
    
    Change-Id: I5d310b11974a8ce16f96be2b81c85fde2edaf4bd
    CRs-Fixed: 4198722

--- components/cp_stats/dispatcher/inc/wlan_cp_stats_mc_defs.h
+++ components/cp_stats/dispatcher/inc/wlan_cp_stats_mc_defs.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -478,6 +478,26 @@ struct vdev_summary_extd_stats {
 };
 
 /**
+ * struct bcn_his_info - beacon history info
+ * @bcn_rssi: beacon rssi
+ * @bcn_tsf: beacon tsf
+ */
+struct bcn_his_info {
+	int32_t bcn_rssi;
+	uint32_t bcn_tsf;
+};
+
+/**
+ * struct recv_bcn_stats - receive beacon stats
+ * @vdev_id: vdev_id
+ * @bcn_history: structure to bcn_his_info
+ */
+struct recv_bcn_stats {
+	uint8_t vdev_id;
+	struct bcn_his_info bcn_history[WMI_MAX_BCN_HISTORY];
+};
+
+/**
  * struct vdev_mc_cp_stats - vdev specific stats
  * @cca: cca stats
  * @tx_rate_flags: tx rate flags (enum tx_rate_info)
@@ -793,6 +813,8 @@ struct peer_stats_info_ext_event {
  * @bcn_protect_stats: pmf bcn protect stats
  * @num_vdev_extd_stats: number of vdev extended stats
  * @vdev_extd_stats: if populated indicates array of ext summary stats per vdev
+ * @num_recv_bcn_stats: number of bcn stats
+ * @bcn_stats: if populated indicates receive beacon stats
  */
 struct stats_event {
 	uint32_t num_pdev_stats;
@@ -824,6 +846,8 @@ struct stats_event {
 	struct pmf_bcn_protect_stats bcn_protect_stats;
 	uint32_t num_vdev_extd_stats;
 	struct vdev_summary_extd_stats *vdev_extd_stats;
+	uint32_t num_recv_bcn_stats;
+	struct recv_bcn_stats *bcn_stats;
 };
 
 /**
