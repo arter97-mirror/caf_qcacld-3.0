e1503bcf40493f775f49071d81758426dfe49748
Surya Prakash Sivaraj <suryapra@qti.qualcomm.com>
2025-06-19 16:26:28 +0530

    qcacld-3.0: Retain the roam context until osif update
    Scenario:
    1) DUT roams using EAPOL Suite-B AKM with 2 links.
    2) For this AKM, EAPOL handling is offloaded to the
    supplicant.
    3) Therefore during roam sync, host driver brings up
    the assoc vdev and updates the link id of all the
    links in sta_ctx.
    4) After EAPOL completion, driver tries to bringup
    the partner vdev. During the cleanup of the old
    partner vdev, the link-id from sta_ctx is cleared out
    for vdev-1.
    5) As part of the successful bring up of the new partner
    vdev, the sta_ctx should be updated with the link ID.
    
    To fix this, prevent clearing the roaming context before
    osif update, and add a callback to clear the roaming
    context after osif update.
    
    Change-Id: I1e9278d4139c737f39c68c306610933418a8ed7e
    CRs-Fixed: 4184876

--- core/hdd/src/wlan_hdd_assoc.c
+++ core/hdd/src/wlan_hdd_assoc.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2012-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for
  * any purpose with or without fee is hereby granted, provided that the
@@ -92,6 +92,7 @@
 #include "wlan_hdd_son.h"
 #include "wlan_dp_ucfg_api.h"
 #include "wlan_cm_ucfg_api.h"
+#include "wlan_mlo_mgr_roam.h"
 
 /* These are needed to recognize WPA and RSN suite types */
 #define HDD_WPA_OUI_SIZE 4
@@ -2958,6 +2959,24 @@ void hdd_roam_profile_init(struct wlan_hdd_link_info *link_info)
 	hdd_exit();
 }
 
+#ifdef WLAN_FEATURE_ROAM_OFFLOAD
+/*
+ * hdd_cm_roam_connect_complete() - Callback to complete roaming
+ * @vdev: pointer to vdev object
+ *
+ * Return: None
+ */
+static void hdd_cm_roam_connect_complete(struct wlan_objmgr_vdev *vdev)
+{
+	mlo_roam_connect_complete(vdev);
+}
+#else
+static inline
+void hdd_cm_roam_connect_complete(struct wlan_objmgr_vdev *vdev)
+{
+}
+#endif
+
 struct osif_cm_ops osif_ops = {
 	.connect_active_notify_cb = hdd_cm_connect_active_notify,
 	.connect_complete_cb = hdd_cm_connect_complete,
@@ -2967,6 +2986,7 @@ struct osif_cm_ops osif_ops = {
 	.save_gtk_cb = hdd_cm_save_gtk,
 #ifdef WLAN_FEATURE_ROAM_OFFLOAD
 	.roam_rt_stats_event_cb = wlan_hdd_cfg80211_roam_events_callback,
+	.roam_complete_notify_cb = hdd_cm_roam_connect_complete,
 #endif
 #ifdef WLAN_FEATURE_FILS_SK
 	.set_hlp_data_cb = hdd_cm_set_hlp_data,
