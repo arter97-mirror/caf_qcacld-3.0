6bc24b77d4b41889ecb32e7c8616c7dae4139169
Srinivas Dasari <dasaris@qti.qualcomm.com>
2025-06-25 15:05:14 +0530

    qcacld-3.0: Skip roam enable when concurrent SAP CSA is pending
    Currently, STA can connect when SAP is in the middle of CSA and
    roaming is also enabled as part of connect procedure. But this
    roam enable allow fw to trigger a roaming and that competes with
    on-going SAP CSA. This might cause to SAP CSA rejection by
    firmware as roaming is in progress and leads to SAP turn-off.
    
    Same is the case when SAP is already present on some channel and
    STA gets connected on another channel on the same MAC. SAP would
    move to STA channel once STA connection is completed but there is
    gap between STA roam enable and SAP decision to move from
    MCC to SCC. If firmware triggers roaming before SAP decides to
    move(SAP attempts to disable the roaming as part of it), SAP fails
    to disable STA roaming and causes contention between STA roaming
    and SAP CSA.
    
    Skip roam enable(RSO_ENABLE) when concurrent SAP CSA is pending or
    in progress. Roaming gets re-enabled fine from
    wlan_hdd_set_roaming_state() on the STA once SAP CSA is done
    
    Change-Id: Ie46ac63fe84607c47024dba60e481ff7ac306d8d
    CRs-Fixed: 4174210

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_get_set_utils.c
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_get_set_utils.c
@@ -14561,3 +14561,24 @@ uint8_t policy_mgr_fetch_scc_vdev_id(struct wlan_objmgr_psoc *psoc,
 
 	return scc_vdev_id;
 }
+
+bool
+policy_mgr_is_conc_sap_ready_for_mcc_to_scc_trans(struct wlan_objmgr_psoc *psoc)
+{
+	qdf_freq_t sap_ch_freq[MAX_NUMBER_OF_CONC_CONNECTIONS];
+	uint8_t vdev_id[MAX_NUMBER_OF_CONC_CONNECTIONS], i, sap_count;
+	uint8_t mcc_to_scc_switch = 0;
+
+	sap_count = policy_mgr_get_mode_specific_conn_info(psoc, sap_ch_freq,
+							   vdev_id,
+							   PM_SAP_MODE);
+	policy_mgr_get_mcc_scc_switch(psoc, &mcc_to_scc_switch);
+
+	for (i = 0; i < sap_count; i++)
+		if (policy_mgr_is_restart_sap_required(psoc, vdev_id[i],
+						       sap_ch_freq[i],
+						       mcc_to_scc_switch))
+			return true;
+
+	return false;
+}
