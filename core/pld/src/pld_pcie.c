f695bb1b1401b2150ec227a35e4c918ee9cbfaa7
Arun Kumar Khandavalli <akhandav@qti.qualcomm.com>
2025-06-27 15:49:42 +0530

    qcacld-3.0: Handle sys reboot notification from platform driver
    In pcie based targets during reboot the pcie is already down
    and the commands sent to the firmware will result into timeout.
    
    To resolve the issue during the reboot notification from platform
    driver handle this as FW_DOWN.
    
    CRs-Fixed: 4171408
    Change-Id: I76253b945b9c0f216195d8eaa8ef43cff238691d

--- core/pld/src/pld_pcie.c
+++ core/pld/src/pld_pcie.c
@@ -303,6 +303,7 @@ static void pld_pcie_uevent(struct pci_dev *pdev,
 		data.uevent = PLD_FW_RECOVERY_START;
 		break;
 	case CNSS_FW_DOWN:
+	case CNSS_SYS_REBOOT:
 		data.uevent = PLD_FW_DOWN;
 		break;
 	default:
