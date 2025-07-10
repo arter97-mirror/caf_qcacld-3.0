2d0757e51f5be61811cae299db838a7dcc2a4a18
Deeksha Gupta <deegupta@qti.qualcomm.com>
2025-07-10 14:39:21 +0530

    qcacld-3.0: Add service cap for TDLS NSS operation
    Add support for service capability for TDLS NSS
    operation.
    
    Change-Id: I56c989a2f4fc1fd10f588b8b23cc9dc0cab1b41c
    CRs-Fixed: 4212732

--- core/wma/src/wma_main.c
+++ core/wma/src/wma_main.c
@@ -5309,6 +5309,32 @@ wma_get_tdls_wideband_support(struct wmi_unified *wmi_handle,
 					     wmi_service_tdls_wideband_support);
 }
 
+#ifdef WLAN_FEATURE_TDLS_NSS_4_4
+/**
+ * wma_get_tdls_nss_confirm_support() - update tgt services with service tdls
+ * NSS operation
+ * @wmi_handle: Unified wmi_handle
+ * @cfg: target services
+ *
+ * Return: none
+ */
+static inline void
+wma_get_tdls_nss_confirm_support(struct wmi_unified *wmi_handle,
+				 struct wma_tgt_services *cfg)
+{
+	cfg->en_tdls_nss_confirm_support =
+				wmi_service_enabled(
+					wmi_handle,
+					wmi_service_tdls_nss_confirm_support);
+}
+#else
+static inline void
+wma_get_tdls_nss_confirm_support(struct wmi_unified *wmi_handle,
+				 struct wma_tgt_services *cfg)
+{
+}
+#endif
+
 #ifdef WLAN_FEATURE_11BE
 /**
  * wma_get_tdls_mlo_support() - update tgt service with service tdls
@@ -5408,6 +5434,11 @@ wma_get_tdls_ax_support(struct wmi_unified *wmi_handle,
 static inline void
 wma_get_tdls_6g_support(struct wmi_unified *wmi_handle,
 			struct wma_tgt_services *cfg)
+{}
+
+static inline void
+wma_get_tdls_nss_confirm_support(struct wmi_unified *wmi_handle,
+				 struct wma_tgt_services *cfg)
 {}
 
 static inline void
@@ -5638,6 +5669,7 @@ static inline void wma_update_target_services(struct wmi_unified *wmi_handle,
 	wma_get_tdls_6g_support(wmi_handle, cfg);
 	wma_get_tdls_wideband_support(wmi_handle, cfg);
 	wma_get_dynamic_vdev_macaddr_support(wmi_handle, cfg);
+	wma_get_tdls_nss_confirm_support(wmi_handle, cfg);
 	wma_get_service_cap_per_link_mlo_stats(wmi_handle, cfg);
 	wma_get_n_link_mlo_support(wmi_handle, cfg);
 	wma_get_mlo_tid_to_link_mapping_support(wmi_handle, cfg);
