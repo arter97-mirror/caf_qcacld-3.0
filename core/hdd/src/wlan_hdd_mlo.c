5147c41e3805b597338288e8c3f321e87314f769
Surya Prakash Sivaraj <suryapra@qti.qualcomm.com>
2025-07-25 11:33:49 +0530

    qcacld-3.0: Add support for link state change event
    Add support to send a vendor event to the userspace to
    update the reason for the link state change along with
    the current active and inactive bitmaps of the affiliated
    MLO links.
    
    Change-Id: I28154696af83b8c9100cf595a83608ebfae4cac0
    CRs-Fixed: 4182057

--- core/hdd/src/wlan_hdd_mlo.c
+++ core/hdd/src/wlan_hdd_mlo.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -261,6 +261,8 @@ static struct mlo_osif_ext_ops mlo_osif_ops = {
 	.mlo_mgr_osif_link_rej_update_mac_addr = hdd_link_rej_mac_addr_update,
 	.mlo_mgr_osif_link_switch_notification =
 					hdd_adapter_link_switch_notification,
+	.mlo_mgr_osif_update_link_state_change =
+					hdd_mlo_update_link_state_change,
 };
 
 QDF_STATUS hdd_mlo_mgr_register_osif_ops(void)
