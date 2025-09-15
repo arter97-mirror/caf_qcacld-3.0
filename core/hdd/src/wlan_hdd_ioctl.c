5b355b5162206eb723f513171b1320b56058cff4
Srinivas Dasari <dasaris@qti.qualcomm.com>
2025-09-15 17:37:50 +0530

    qcacld-3.0: Disable IDLE roaming upon pwr_save/SUSPEND cmd reset
    Currently, IDLE roaming is disabled only when both pwr_save and
    SUSPEND are reset by userspace. But the expectation is to enable
    IDLE roaming only when both are set and reset in all other cases.
    So, disable IDLE roaming upon pwr_save/SUSPEND reset.
    Also, clear the bitmap upon disconnect.
    
    Change-Id: I885000de26906bf3a0d82a1cd6cdabcf842508a8
    CRs-Fixed: 4291334

--- core/hdd/src/wlan_hdd_ioctl.c
+++ core/hdd/src/wlan_hdd_ioctl.c
@@ -3280,6 +3280,7 @@ static int drv_cmd_set_suspend_mode(struct wlan_hdd_link_info *link_info,
 	int errno;
 	uint8_t *value = command;
 	uint8_t idle_monitor;
+	QDF_STATUS status;
 
 	if (QDF_STA_MODE != adapter->device_mode) {
 		hdd_debug("Non-STA interface");
@@ -3304,6 +3305,15 @@ static int drv_cmd_set_suspend_mode(struct wlan_hdd_link_info *link_info,
 		  idle_monitor,
 		  ucfg_pmo_is_configure_apf_per_screen_state(hdd_ctx->psoc));
 
+	status = hdd_update_send_idle_roam_bitmap(link_info, hdd_ctx,
+						  idle_monitor,
+						  IDLE_ROAM_SETSUSPEND_CMD);
+
+	if (QDF_IS_STATUS_ERROR(status)) {
+		hdd_debug("Send suspend mode to fw failed");
+		return -EINVAL;
+	}
+
 	if (sme_get_dhcp_status(hdd_ctx->mac_handle, link_info->vdev_id) &&
 	    idle_monitor == 1) {
 		hdd_nofl_debug("DHCP in progress. Ignore SETSUSPEND command");
@@ -3318,7 +3328,6 @@ int hdd_handle_apf_mode_on_idle(struct hdd_context *hdd_ctx,
 				struct wlan_hdd_link_info *link_info,
 				uint8_t idle_monitor)
 {
-	QDF_STATUS status;
 	struct hdd_adapter *adapter = link_info->adapter;
 	struct wlan_objmgr_vdev *vdev;
 
@@ -3347,14 +3356,6 @@ int hdd_handle_apf_mode_on_idle(struct hdd_context *hdd_ctx,
 		}
 	}
 
-	status = hdd_update_send_idle_roam_bitmap(link_info, hdd_ctx,
-						  idle_monitor,
-						  IDLE_ROAM_SETSUSPEND_CMD);
-
-	if (QDF_IS_STATUS_ERROR(status)) {
-		hdd_debug("Send suspend mode to fw failed");
-		return -EINVAL;
-	}
 	return 0;
 }
 
