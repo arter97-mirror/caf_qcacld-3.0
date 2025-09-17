70c8df653a9f084d29eb1ea65d973e6a9a2e7416
Qun Zhang <qunz@qti.qualcomm.com>
2025-09-17 10:44:49 +0800

    qcacld-3.0: Fix symbolic link file cannot be packed
    Symbolic link files cannot be packed as part of
    vendor.img in Android W build, that causes wlan
    driver loading failed due to some necessary files
    missed. Add symbolic link files to
    ALL_DEFAULT_INSTALLED_MODULES to make sure symbolic
    link file can be packed as expected.
    
    Change-Id: Ide4feeb9fcc82c825b90835d78fed9ac9f328cf7
    CRs-Fixed: 4298068

--- Android.mk
+++ Android.mk
@@ -179,6 +179,7 @@ endif
 # Create wlan_mac.bin symbolic link as part of the module
 $(call symlink-file,,$(TARGET_MAC_BIN_PATH)/wlan_mac.bin,$(TARGET_FW_PATH)/wlan_mac.bin)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_FW_PATH)/wlan_mac.bin
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_FW_PATH)/wlan_mac.bin
 
 # Conditionally create module symbolic link
 ifneq ($(findstring $(WLAN_CHIPSET),$(WIFI_DRIVER_DEFAULT)),)
@@ -186,10 +187,12 @@ ifeq ($(PRODUCT_VENDOR_MOVE_ENABLED),true)
 ifneq ($(WIFI_DRIVER_INSTALL_TO_KERNEL_OUT),true)
 $(call symlink-file,,$(TARGET_COPY_OUT_VENDOR)/lib/modules/$(WLAN_CHIPSET)/$(LOCAL_MODULE),$(TARGET_OUT_VENDOR)/lib/modules/$(LOCAL_MODULE))
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_VENDOR)/lib/modules/$(LOCAL_MODULE)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib/modules/$(LOCAL_MODULE)
 endif
 else
 $(call symlink-file,,/system/lib/modules/$(WLAN_CHIPSET)/$(LOCAL_MODULE),$(TARGET_OUT)/lib/modules/$(LOCAL_MODULE))
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT)/lib/modules/$(LOCAL_MODULE)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT)/lib/modules/$(LOCAL_MODULE)
 endif
 endif
 
@@ -197,11 +200,13 @@ endif
 ifeq ($(TARGET_BOARD_AUTO),true)
 $(call symlink-file,,$(TARGET_CFG_PATH)/WCNSS_qcom_cfg.ini,$(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini)
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini
 $(call wlog,"generate soft link because TARGET_BOARD_AUTO true")
 else
 ifneq ($(GENERIC_ODM_IMAGE),true)
 $(call symlink-file,,$(TARGET_CFG_PATH)/WCNSS_qcom_cfg.ini,$(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini)
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini
 $(call wlog,"generate soft link because GENERIC_ODM_IMAGE not true")
 endif
 endif
@@ -382,6 +387,7 @@ endif
 # Create wlan_mac.bin symbolic link as part of the module
 $(call symlink-file,,$(TARGET_MAC_BIN_PATH)/wlan_mac.bin,$(TARGET_FW_PATH)/wlan_mac.bin)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_FW_PATH)/wlan_mac.bin
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_FW_PATH)/wlan_mac.bin
 
 # Conditionally create module symbolic link
 ifneq ($(findstring $(WLAN_CHIPSET),$(WIFI_DRIVER_DEFAULT)),)
@@ -389,10 +395,12 @@ ifeq ($(PRODUCT_VENDOR_MOVE_ENABLED),true)
 ifneq ($(WIFI_DRIVER_INSTALL_TO_KERNEL_OUT),true)
 $(call symlink-file,,$(TARGET_COPY_OUT_VENDOR)/lib/modules/$(WLAN_CHIPSET)/$(LOCAL_MODULE),$(TARGET_OUT_VENDOR)/lib/modules/$(LOCAL_MODULE))
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_VENDOR)/lib/modules/$(LOCAL_MODULE)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_VENDOR)/lib/modules/$(LOCAL_MODULE)
 endif
 else
 $(call symlink-file,,/system/lib/modules/$(WLAN_CHIPSET)/$(LOCAL_MODULE),$(TARGET_OUT)/lib/modules/$(LOCAL_MODULE))
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT)/lib/modules/$(LOCAL_MODULE)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT)/lib/modules/$(LOCAL_MODULE)
 endif
 endif
 
@@ -400,11 +408,13 @@ endif
 ifeq ($(TARGET_BOARD_AUTO),true)
 $(call symlink-file,,$(TARGET_CFG_PATH)/WCNSS_qcom_cfg.ini,$(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini)
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini
 $(call wlog,"generate soft link because TARGET_BOARD_AUTO true")
 else
 ifneq ($(GENERIC_ODM_IMAGE),true)
 $(call symlink-file,,$(TARGET_CFG_PATH)/WCNSS_qcom_cfg.ini,$(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini)
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_FW_PATH)/WCNSS_qcom_cfg.ini
 $(call wlog,"generate soft link because GENERIC_ODM_IMAGE not true")
 endif
 endif
