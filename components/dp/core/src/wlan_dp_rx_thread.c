f838db6caa76635f10daa331aad5f8ffc257c414
Jinwei Chen <jinweic@qti.qualcomm.com>
2025-09-18 06:54:38 -0700

    qcacld-3.0: Fix warning caused by missing napi_disable()
    In dp_rx_tm_thread_napi_init(), napi_enable() is called for
    rx_thread->napi. However, during dp_rx_tm_thread_napi_deinit(),
    there is no corresponding napi_disable() before netif_napi_del()
    is invoked. This leads to a kernel warning due to an invalid
    napi_struct state.
    
    To fix this, napi_disable() is added in dp_rx_tm_thread_napi_deinit()
    to properly pair with napi_enable().
    
    Change-Id: I44c006bab56498e22be1e50fe0fe380d27559dab
    CRs-Fixed: 4296398

--- components/dp/core/src/wlan_dp_rx_thread.c
+++ components/dp/core/src/wlan_dp_rx_thread.c
@@ -923,6 +923,7 @@ static void dp_rx_tm_thread_napi_deinit(struct dp_rx_thread *rx_thread)
 	struct net_device *dummy_nd;
 
 	dummy_nd = dp_rx_thread_get_dummy_netdev_ptr(rx_thread);
+	qdf_napi_disable(&rx_thread->napi);
 	qdf_netif_napi_del(&rx_thread->napi);
 	qdf_net_if_destroy_dummy_if((struct qdf_net_if *)dummy_nd);
 	dp_rx_thread_set_dummy_netdev_ptr(rx_thread, NULL);
