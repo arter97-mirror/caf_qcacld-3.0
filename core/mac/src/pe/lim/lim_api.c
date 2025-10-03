5c34c7bbf3048c5b1ad30543b579cc702749deb7
Surya Prakash Sivaraj <suryapra@qti.qualcomm.com>
2025-10-03 10:37:44 +0530

    qcacld-3.0: Fix potential OOB access in roam sync
    The reassoc request frame from firmware could be
    less than the size of the MAC header, in which case
    there is scope for OOB access.
    
    To fix this, sanitize the reassoc request before access.
    
    Also, the current logic is to parse SSID IE from only the
    reassoc request frame. But, the firmware can use assoc req
    frames as well during roam. Therefore, extend this logic
    to parse SSID IE from assoc request frames as well.
    
    Change-Id: Ie0193fc06759b4c6954246b9d8f79a4885bbd7eb
    CRs-Fixed: 4309476

--- core/mac/src/pe/lim/lim_api.c
+++ core/mac/src/pe/lim/lim_api.c
@@ -2431,22 +2431,46 @@ lim_roam_fill_bss_descr(struct mac_context *mac,
 	struct element_info frame;
 	struct cm_roam_values_copy mdie_cfg = {0};
 	uint8_t *ssid_ie = NULL;
+	uint8_t ies_offset =  WLAN_REASSOC_REQ_IES_OFFSET;
+	tpSirMacMgmtHdr hdr;
 
 	bcn_proberesp_ptr = (uint8_t *)roam_synch_ind +
 		roam_synch_ind->beacon_probe_resp_offset;
 	bcn_proberesp_len = roam_synch_ind->beacon_probe_resp_length;
 
+	if (roam_synch_ind->reassoc_req_length <= sizeof(tSirMacMgmtHdr)) {
+		pe_err("Invalid reassoc req with length %d",
+		       roam_synch_ind->reassoc_req_length);
+		return QDF_STATUS_E_FAILURE;
+	}
+
+	hdr = (tpSirMacMgmtHdr)(uint8_t *)roam_synch_ind +
+					roam_synch_ind->reassoc_req_offset;
+	if (hdr->fc.type == WLAN_FC0_TYPE_MGMT &&
+	    hdr->fc.subType == SIR_MAC_MGMT_ASSOC_REQ)
+		ies_offset = WLAN_ASSOC_REQ_IES_OFFSET;
+
 	reassoc_req_ies = (uint8_t *)roam_synch_ind +
-			roam_synch_ind->reassoc_req_offset +
-			sizeof(tSirMacMgmtHdr) + WLAN_REASSOC_REQ_IES_OFFSET;
+				roam_synch_ind->reassoc_req_offset +
+				sizeof(tSirMacMgmtHdr) + ies_offset;
+
+	if (roam_synch_ind->reassoc_req_length <=
+				sizeof(tSirMacMgmtHdr) + ies_offset) {
+		pe_err("Invalid reassoc req with length %d",
+		       roam_synch_ind->reassoc_req_length);
+		return QDF_STATUS_E_FAILURE;
+	}
+
 	reassoc_req_ies_len = roam_synch_ind->reassoc_req_length -
-			sizeof(tSirMacMgmtHdr) - WLAN_REASSOC_REQ_IES_OFFSET;
+				sizeof(tSirMacMgmtHdr) - ies_offset;
 
 	ssid_ie = (uint8_t *)wlan_get_ie_ptr_from_eid(WLAN_ELEMID_SSID,
 						      reassoc_req_ies,
 						      reassoc_req_ies_len);
-	if (!ssid_ie)
+	if (!ssid_ie) {
+		pe_err("SSID IE is missing in (re)assoc request frame");
 		return QDF_STATUS_E_FAILURE;
+	}
 
 	frame.ptr = NULL;
 	frame.len = 0;
