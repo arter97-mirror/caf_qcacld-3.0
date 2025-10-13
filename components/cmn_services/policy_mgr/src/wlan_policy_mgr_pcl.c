dfff7a4a3b5a5b34cdb127e07c809ecc44ee92f5
Will Huang <wilhuang@qti.qualcomm.com>
2025-10-13 16:09:50 +0530

    qcacld-3.0: Add MCC check for SAP when more than 4 connections
    Currently if connection number bigger or equal 4 also invoke
    policy_mgr_modify_sap_go_4th_conc_disallow() to filter PCL channels.
    It may not sufficient because MCC is not allowed for SAP.
    
    Add logic to first check PCL channels if MCC will trigger when
    connection number is more than 4 for SAP mode.
    
    Change-Id: Ie6971ecd0990047ec6d7cb3bfb9127789ed5ce83
    CRs-Fixed: 4304121

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_pcl.c
@@ -1535,8 +1535,63 @@ policy_mgr_modify_dual_sap_band_pcl_filter(
 	return QDF_STATUS_SUCCESS;
 }
 
+#if defined(FEATURE_FIFTH_CONNECTION) || defined(FEATURE_SIXTH_CONNECTION)
 /**
- * policy_mgr_modify_sap_go_4th_conc_disallow() - filter out channel that
+ * policy_mgr_allow_4more_new_freq() - function to check if new channel
+ * frequency is allowed when connection number is equal or greater than 4
+ * @psoc: pointer to soc
+ * @ch_freq: input channel frequency to check
+ * @mode: operating mode of new connection
+ *
+ * Return: true if allowed otherwise false
+ */
+static bool
+policy_mgr_allow_4more_new_freq(struct wlan_objmgr_psoc *psoc,
+				qdf_freq_t ch_freq,
+				enum policy_mgr_con_mode mode)
+{
+	uint32_t idx;
+	bool mcc;
+	qdf_freq_t conn_freq;
+
+	if (mode != PM_SAP_MODE)
+		return true;
+
+	/* mutex already hold by outside */
+	for (idx = 0; idx < MAX_NUMBER_OF_CONC_CONNECTIONS; idx++) {
+		if (!pm_conc_connection_list[idx].in_use)
+			continue;
+
+		conn_freq = pm_conc_connection_list[idx].freq;
+		if (ch_freq == conn_freq)
+			continue;
+
+		if (policy_mgr_is_current_hwmode_sbs(psoc))
+			mcc = policy_mgr_2_freq_same_mac_in_sbs(psoc,
+								ch_freq,
+								conn_freq);
+		else
+			mcc = policy_mgr_2_freq_same_mac_in_dbs(psoc,
+								ch_freq,
+								conn_freq);
+		if (mcc)
+			return false;
+	}
+
+	return true;
+}
+#else
+static inline bool
+policy_mgr_allow_4more_new_freq(struct wlan_objmgr_psoc *psoc,
+				qdf_freq_t ch_freq,
+				enum policy_mgr_con_mode mode)
+{
+	return false;
+}
+#endif
+
+/**
+ * policy_mgr_modify_sap_go_conc_disallow() - filter out channel that
  * is not allowed for 4th sap/go connection
  * @psoc: pointer to soc
  * @mode: interface mode
@@ -1546,7 +1601,7 @@ policy_mgr_modify_dual_sap_band_pcl_filter(
  *
  * Return: QDF_STATUS
  */
-static QDF_STATUS policy_mgr_modify_sap_go_4th_conc_disallow(
+static QDF_STATUS policy_mgr_modify_sap_go_conc_disallow(
 		struct wlan_objmgr_psoc *psoc,
 		enum policy_mgr_con_mode mode,
 		uint32_t *pcl_list_org,
@@ -1572,6 +1627,12 @@ static QDF_STATUS policy_mgr_modify_sap_go_4th_conc_disallow(
 		goto end;
 
 	for (i = 0; i < *pcl_len_org; i++) {
+		if (num_connections >= 4 &&
+		    !policy_mgr_allow_4more_new_freq(psoc,
+						     pcl_list_org[i],
+						     mode))
+			continue;
+
 		if (policy_mgr_allow_4th_new_freq(psoc, pcl_list_org[i],
 						  mode, 0)) {
 			pcl_list_org[pcl_len] = pcl_list_org[i];
@@ -1757,10 +1818,10 @@ static QDF_STATUS policy_mgr_pcl_modification_for_sap(
 		pcl_len = *len;
 	}
 
-	status = policy_mgr_modify_sap_go_4th_conc_disallow(psoc,
-							    PM_SAP_MODE,
-							    pcl_channels,
-							    pcl_weight, len);
+	status = policy_mgr_modify_sap_go_conc_disallow(psoc,
+							PM_SAP_MODE,
+							pcl_channels,
+							pcl_weight, len);
 	if (QDF_IS_STATUS_ERROR(status)) {
 		policy_mgr_err("failed to modify pcl for 4th sap channels");
 		return status;
@@ -1867,10 +1928,10 @@ static QDF_STATUS policy_mgr_pcl_modification_for_p2p_go(
 		srd_modified_pcl = true;
 		pcl_len = *len;
 	}
-	status = policy_mgr_modify_sap_go_4th_conc_disallow(psoc,
-							    PM_P2P_GO_MODE,
-							    pcl_channels,
-							    pcl_weight, len);
+	status = policy_mgr_modify_sap_go_conc_disallow(psoc,
+							PM_P2P_GO_MODE,
+							pcl_channels,
+							pcl_weight, len);
 	if (QDF_IS_STATUS_ERROR(status)) {
 		policy_mgr_err("failed to modify pcl for 4th go channels");
 		return status;
