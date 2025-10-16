a26fb402e2dd8a8e19c46c98e1e7a6d1dfccb743
Sanskar Jain <sansjain@qti.qualcomm.com>
2025-10-16 10:50:53 +0530

    qcacld-3.0: Assign NULL to memory location after freeing it
    After freeing the memory for *entries array it is not assigned
    to NULL. This can lead to use_after_free of memory and can
    cause segmentation fault.
    
    Assign *entries pointer to NULL after freeing the memory for it.
    
    CRs-Fixed: 4315444
    Change-Id: Iec98523186a406b75aef65ce7bcd26f4063daeab

--- components/wmi/src/wmi_unified_roam_tlv.c
+++ components/wmi/src/wmi_unified_roam_tlv.c
@@ -4184,6 +4184,7 @@ free_entries:
 	qdf_mem_zero(*entries,
 		     WLAN_MAX_ML_BSS_LINKS * sizeof(**entries));
 	qdf_mem_free(*entries);
+	*entries = NULL;
 
 free_keys:
 	for (k = 0; k < WMI_NUM_KEYS_ALLOCATED; k++) {
