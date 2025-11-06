Guxin12/android_kernel_xiaomi_marble

qcacld-3.0: Guard assoc_link_id for kernel >= 6.3.0
In the fallback implementation of hdd_ml_sap_owe_fill_ml_info,
added a kernel version check to conditionally set assoc_link_id
only when building against Linux kernel 6.3.0 or newer. This
prevents compilation errors on older kernels where the field
is not defined.

Change-Id: I2393b0ee910aac1938daf410687a220bf6d22fc8
CRs-Fixed: 4320889

core/hdd/src/wlan_hdd_cfg80211.c
@@ -24949,6 +24949,7 @@ hdd_ml_sap_owe_fill_ml_info(struct hdd_adapter *adapter,
 	wlan_objmgr_peer_release_ref(peer, WLAN_HDD_ID_OBJ_MGR);
 }
 #else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0))
 static void
 hdd_ml_sap_owe_fill_ml_info(struct hdd_adapter *adapter,
 			    struct cfg80211_update_owe_info *owe_info,
@@ -24956,6 +24957,14 @@ hdd_ml_sap_owe_fill_ml_info(struct hdd_adapter *adapter,
 {
 	owe_info->assoc_link_id = -1;
 }
+#else
+static void
+hdd_ml_sap_owe_fill_ml_info(struct hdd_adapter *adapter,
+			    struct cfg80211_update_owe_info *owe_info,
+			    uint8_t *peer_mac)
+{
+}
+#endif
 #endif
 
 void hdd_send_update_owe_info_event(struct hdd_adapter *adapter,

arter97-mirror/caf_qcacld-3.0

core/hdd/src/wlan_hdd_cfg80211.c
@@ -34275,6 +34275,7 @@ hdd_ml_sap_owe_fill_ml_info(struct wlan_hdd_link_info *link_info,
 	wlan_objmgr_peer_release_ref(peer, WLAN_HDD_ID_OBJ_MGR);
 }
 #else
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0))
 static void
 hdd_ml_sap_owe_fill_ml_info(struct wlan_hdd_link_info *link_info,
 			    struct cfg80211_update_owe_info *owe_info,
@@ -34282,6 +34283,14 @@ hdd_ml_sap_owe_fill_ml_info(struct wlan_hdd_link_info *link_info,
 {
 	owe_info->assoc_link_id = -1;
 }
+#else
+static void
+hdd_ml_sap_owe_fill_ml_info(struct wlan_hdd_link_info *link_info,
+			    struct cfg80211_update_owe_info *owe_info,
+			    uint8_t *peer_mac)
+{
+}
+#endif
 #endif
 
 void hdd_send_update_owe_info_event(struct wlan_hdd_link_info *link_info,