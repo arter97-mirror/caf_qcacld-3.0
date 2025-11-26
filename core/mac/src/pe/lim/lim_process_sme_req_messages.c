28339febc17a055b18491fe942057d7562c9c76b
Liangwei Dong <liangwei@qti.qualcomm.com>
2025-11-26 16:23:11 +0800

    qcacld-3.0: Correct vht_ext_nss_bw_capable field overwritten issue
    When updating txHighest field of tSirVhtMcsInfo for max NSS supported
    in API lim_populate_mcs_set_vht_per_vdev, the vht_extended_nss_bw_cap
    field is overwritten by return value of
    VHT_GET_DATARATE_FOR_NSS_AND_GI(tx_nss, true).
    
    Fix by updating the txSupDataRate field only instead of overwriting
    txHighest.
    
    Change-Id: I686b3e5492186a8d9381591c22b3ebea8da18a12
    CRs-Fixed: 4360879

--- core/mac/src/pe/lim/lim_process_sme_req_messages.c
+++ core/mac/src/pe/lim/lim_process_sme_req_messages.c
@@ -9630,6 +9630,11 @@ static void lim_set_pdev_vht_ie(struct mac_context *mac_ctx, uint8_t pdev_id,
 	tSirMacVHTCapabilityInfo *vht_cap;
 	int i;
 	tSirVhtMcsInfo *vht_mcs;
+	union {
+		uint16_t		       u_value;
+		tSirMacVHTRxSupDataRateInfo    vht_rx_supp_rate;
+		tSirMacVHTTxSupDataRateInfo    vht_tx_supp_rate;
+	} u_vht_data_rate_info;
 
 	for (i = 1; i <= nss; i++) {
 		ie_params = qdf_mem_malloc(sizeof(*ie_params));
@@ -9665,11 +9670,15 @@ static void lim_set_pdev_vht_ie(struct mac_context *mac_ctx, uint8_t pdev_id,
 				(tSirVhtMcsInfo *)&p_ie[2 +
 				sizeof(tSirMacVHTCapabilityInfo)];
 			vht_mcs->rxMcsMap |= VHT_DISABLE_MCS_OVER_NSS(i);
-			vht_mcs->rxHighest =
+			u_vht_data_rate_info.u_value = vht_mcs->rxHighest;
+			u_vht_data_rate_info.vht_rx_supp_rate.rxSupDataRate =
 				VHT_GET_DATARATE_FOR_NSS_AND_GI(i, true);
+			vht_mcs->rxHighest = u_vht_data_rate_info.u_value;
 			vht_mcs->txMcsMap |= VHT_DISABLE_MCS_OVER_NSS(i);
-			vht_mcs->txHighest =
+			u_vht_data_rate_info.u_value = vht_mcs->txHighest;
+			u_vht_data_rate_info.vht_tx_supp_rate.txSupDataRate =
 				VHT_GET_DATARATE_FOR_NSS_AND_GI(i, true);
+			vht_mcs->txHighest = u_vht_data_rate_info.u_value;
 		}
 		msg.type = WMA_SET_PDEV_IE_REQ;
 		msg.bodyptr = ie_params;
--- core/mac/src/pe/lim/lim_utils.c
+++ core/mac/src/pe/lim/lim_utils.c
@@ -5965,6 +5965,12 @@ static void lim_populate_mcs_set_vht_per_vdev(struct mac_context *mac_ctx,
 {
 	struct wlan_mlme_nss_chains *nss_chains_ini_cfg;
 	tSirVhtMcsInfo *vht_mcs;
+	union {
+		uint16_t		       u_value;
+		tSirMacVHTRxSupDataRateInfo    vht_rx_supp_rate;
+		tSirMacVHTTxSupDataRateInfo    vht_tx_supp_rate;
+	} u_vht_data_rate_info;
+
 	struct wlan_objmgr_vdev *vdev =
 			wlan_objmgr_get_vdev_by_id_from_psoc(mac_ctx->psoc,
 							     vdev_id,
@@ -5985,16 +5991,19 @@ static void lim_populate_mcs_set_vht_per_vdev(struct mac_context *mac_ctx,
 	/* Populate VHT MCS Information */
 	vht_mcs->txMcsMap |=
 		VHT_DISABLE_MCS_OVER_NSS(nss_chains_ini_cfg->tx_nss[band]);
-	vht_mcs->txHighest =
+	u_vht_data_rate_info.u_value = vht_mcs->txHighest;
+	u_vht_data_rate_info.vht_tx_supp_rate.txSupDataRate =
 		VHT_GET_DATARATE_FOR_NSS_AND_GI(nss_chains_ini_cfg->tx_nss[band],
 						true);
+	vht_mcs->txHighest = u_vht_data_rate_info.u_value;
 
-	/* Populate VHT MCS Information */
 	vht_mcs->rxMcsMap |=
 		VHT_DISABLE_MCS_OVER_NSS(nss_chains_ini_cfg->rx_nss[band]);
-	vht_mcs->rxHighest =
+	u_vht_data_rate_info.u_value = vht_mcs->rxHighest;
+	u_vht_data_rate_info.vht_rx_supp_rate.rxSupDataRate =
 		VHT_GET_DATARATE_FOR_NSS_AND_GI(nss_chains_ini_cfg->rx_nss[band],
 						true);
+	vht_mcs->rxHighest = u_vht_data_rate_info.u_value;
 
 end:
 	wlan_objmgr_vdev_release_ref(vdev, WLAN_MLME_SB_ID);
