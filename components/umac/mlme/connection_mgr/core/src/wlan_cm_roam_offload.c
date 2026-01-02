cf293f63cc3d4404ba14ae596bea727add329e8a
Vinod Kumar Pirla <vpirla@qti.qualcomm.com>
2026-01-02 02:07:38 -0800

    qcacld-3.0: Always scan on roam invoke to non-Tx BSSID
    When FAST REASSOC is requested, host sends target AP’s BSSID
    and frequency via WMI_ROAM_INVOKE_CMDID. If the beacon template
    exists in the scan cache, host sets roam_scan_mode to SKIP to
    avoid FW scanning. For non-Tx BSSID in an MBSSID set, FW needs
    the Tx BSSID to filter beacons correctly. If host set skip
    scanning in this roam invoke command and provides only the
    non-Tx beacon template, FW cannot derive Tx BSSID, causing
    beacon filter mismatch and resource lock issues.
    
    In race conditions with roam invoke and WOW enable, this can
    lead to timer expiry and assertion failures.
    
    If target AP is a non-Tx BSSID, ignore its beacon template
    and allow FW to scan the channel to learn Tx/non-Tx BSSIDs,
    ensuring proper beacon filtering and resource release.
    
    Change-Id: I5c0ede622f5d0401f50af5f02fecec73f95e0707
    CRs-Fixed: 4395339

--- components/umac/mlme/connection_mgr/core/src/wlan_cm_roam_offload.c
+++ components/umac/mlme/connection_mgr/core/src/wlan_cm_roam_offload.c
@@ -6956,6 +6956,7 @@ cm_find_roam_candidate(struct wlan_objmgr_pdev *pdev,
 		       struct cm_roam_req *roam_req,
 		       struct roam_invoke_req *roam_invoke_req)
 {
+	QDF_STATUS status;
 	struct scan_filter *filter;
 	qdf_list_t *candidate_list;
 	uint32_t num_bss = 0;
@@ -7000,30 +7001,46 @@ cm_find_roam_candidate(struct wlan_objmgr_pdev *pdev,
 		return QDF_STATUS_E_EMPTY;
 	}
 
-	qdf_list_peek_front(candidate_list, &cur_node);
+	status = qdf_list_peek_front(candidate_list, &cur_node);
+	if (QDF_IS_STATUS_ERROR(status)) {
+		mlme_debug(CM_PREFIX_FMT "Failed to get node from list",
+			   CM_PREFIX_REF(roam_req->req.vdev_id,
+					 roam_req->cm_id));
+		goto purge_list;
+	}
+
 	candidate = qdf_container_of(cur_node,
-				     struct scan_cache_node,
-				     node);
+				     struct scan_cache_node, node);
+
+	if (util_is_scan_entry_non_tx_bssid(candidate->entry)) {
+		mlme_debug(CM_PREFIX_FMT "Drop scan entry for non-Tx BSSID",
+			   CM_PREFIX_REF(roam_req->req.vdev_id,
+					 roam_req->cm_id));
+		status = QDF_STATUS_E_EMPTY;
+		goto purge_list;
+	}
 
 	roam_invoke_req->frame_len = candidate->entry->raw_frame.len;
-
-	if (!roam_invoke_req->frame_len)
-		return QDF_STATUS_E_INVAL;
+	if (!roam_invoke_req->frame_len) {
+		status = QDF_STATUS_E_INVAL;
+		goto purge_list;
+	}
 
 	roam_invoke_req->frame_buf = qdf_mem_malloc(roam_invoke_req->frame_len);
-
 	if (!roam_invoke_req->frame_buf) {
 		roam_invoke_req->frame_len = 0;
-		return QDF_STATUS_E_NOMEM;
+		status = QDF_STATUS_E_NOMEM;
+		goto purge_list;
 	}
 
 	qdf_mem_copy(roam_invoke_req->frame_buf,
 		     candidate->entry->raw_frame.ptr,
 		     roam_invoke_req->frame_len);
 
+purge_list:
 	wlan_scan_purge_results(candidate_list);
 
-	return QDF_STATUS_SUCCESS;
+	return status;
 }
 
 #if (defined(CONNECTIVITY_DIAG_EVENT) && defined(WLAN_FEATURE_ROAM_OFFLOAD))
