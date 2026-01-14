4f6f79c1958645a9460c45de9e2250d53dbdeeae
gongx <gongx@qti.qualcomm.com>
2026-01-14 16:31:03 +0800

    qcacld-3.0: update version as PostCS11
    Change-Id: Ia4199d6d3bb8c60cafd8818cdad48ce10df8de4e

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,7 +32,7 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "PostCS-10"
+#define QWLAN_VERSION_EXTRA            "PostCS-11"
 #define QWLAN_VERSION_BUILD            131
 
 #define QWLAN_VERSIONSTR               "5.2.1.131-"QWLAN_VERSION_EXTRA
