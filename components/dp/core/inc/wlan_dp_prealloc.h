b0c7e85d28c1fff0421649a0be482f20ddc251b0
Arsalan Sariya <asariya@qti.qualcomm.com>
2026-02-22 20:36:15 +0530

    qcacld-3.0: Add changes for tracking prealloc rx page pools
    Add changes to make existing prealloc calls compatible
    to change in page pool apis for tracking in-flight
    buffers.
    
    Change-Id: I59df2e45d9512ff79248d27f63c887b6a7ee9c52
    CRs-Fixed: 4449000

--- components/dp/core/inc/wlan_dp_prealloc.h
+++ components/dp/core/inc/wlan_dp_prealloc.h
@@ -153,12 +153,13 @@ void dp_prealloc_put_consistent_mem_unaligned(void *va_unaligned);
  * @type: page pool type whether tx or rx page pool
  * @pool_size: requested page pool size
  * @dynamic_pp_request: Dynamic Page pool request
+ * @pp_track_id: Pool track id pointer to track in-flight buffers
  *
  * Return: pointer to dp_page_pool_t on success or NULL on failure
  */
 struct dp_page_pool_t*
 dp_prealloc_get_page_pool(enum qdf_dp_tx_pp_type type, uint32_t pool_size,
-			  bool dynamic_pp_request);
+			  bool dynamic_pp_request, int *pp_track_id);
 
 /**
  * dp_prealloc_put_page_pool() - put page pool reference back to pre-alloc pool
