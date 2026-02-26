242d85b6c8abbcaa4e06c7705404fbe8b090d741
Aasir Rasheed <arasheed@qti.qualcomm.com>
2026-02-26 00:47:04 -0800

    qcacld-3.0: Set link bitmap for all links in mixed mode
    Currently, the host driver sets the link bitmap based only on the
    first two or three links requested by the user from the link info.
    
    This behavior is incorrect because the link info can change at any
    time, and restricting link selection at the host side is not reliable.
    
    Link selection in mixed/active mode should be handled by the firmware,
    which has the correct visibility to decide which links should be activated.
    
    Update the host behavior to set the bitmap for all available links when
    sending the set link command from userspace, allowing the firmware
    to select the appropriate links to activate.
    
    Change-Id: Ie100cdbbe191ec8d4a2c348fb850155b0783c7ec
    CRs-Fixed: 4453032

--- components/umac/mlme/mlo_mgr/src/wlan_mlo_link_force.c
+++ components/umac/mlme/mlo_mgr/src/wlan_mlo_link_force.c
@@ -53,6 +53,9 @@
 /*Max allowed forced links*/
 #define MAX_FORCE_ACTIVE_LINKS         2
 
+/* QCA_WLAN_VENDOR_LINK_STATE_CONTROL_MODE_MIXED */
+#define CONTROL_MODE_MIXED	       2
+
 static uint32_t
 ml_nlink_set_emlsr_mode_disable_req(struct wlan_objmgr_psoc *psoc,
 				    struct wlan_objmgr_vdev *vdev,
@@ -4778,8 +4781,13 @@ ml_nlink_validate_request(struct wlan_objmgr_psoc *psoc,
 				  source, request->force_active_num);
 			return false;
 		}
+
+		/* Avoid check for CONTROL_MODE_MIXED, as force_active_num
+		 * will come from userspace and fw expects to set all the link.
+		 */
 		if (request->force_active_num > 1 &&
-		    combined->force_inactive_num) {
+		    combined->force_inactive_num &&
+		    request->link_ctrl_mode != CONTROL_MODE_MIXED) {
 			link_bitmap = ~request->force_active_num_bitmap &
 				combined->force_inactive_num_bitmap;
 			if (!link_bitmap) {
@@ -6072,6 +6080,7 @@ ml_nlink_vendor_cmd_handler(struct wlan_objmgr_psoc *psoc,
 						SET_LINK_FROM_VENDOR_CMD);
 		break;
 	case LINK_CONTROL_MODE_USER:
+		req.link_ctrl_mode = data->evt.vendor.link_ctrl_mode;
 		req.mode = data->evt.vendor.mode;
 		req.reason = data->evt.vendor.reason;
 		req.force_active_bitmap = data->evt.vendor.link_bitmap;
@@ -6086,6 +6095,7 @@ ml_nlink_vendor_cmd_handler(struct wlan_objmgr_psoc *psoc,
 						   SET_LINK_FROM_VENDOR_CMD);
 		break;
 	case LINK_CONTROL_MODE_MIXED:
+		req.link_ctrl_mode = data->evt.vendor.link_ctrl_mode;
 		req.mode = data->evt.vendor.mode;
 		req.reason = data->evt.vendor.reason;
 		req.force_active_num = data->evt.vendor.link_num;
