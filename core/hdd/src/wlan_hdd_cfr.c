34088472f7108ef5ad28d529f446a42e63abc13a
Aasir Rasheed <arasheed@qti.qualcomm.com>
2026-03-08 23:19:30 -0700

    qcacld-3.0: Convert ucode LTF type to vendor LTF type
    The host driver currently uses the ucode LTF type and forwards it
    directly to userspace. Switch to using the vendor-specific LTF type
    and send that to userspace instead.
    
    Change-Id: I6331df34e7db707ac89523e4edb1be0a7d1bc56b
    CRs-Fixed: 4463024

--- core/hdd/src/wlan_hdd_cfr.c
+++ core/hdd/src/wlan_hdd_cfr.c
@@ -225,6 +225,26 @@ nl80211_chan_width convert_ucode_bw_to_nl_bw(uint8_t bw)
 	}
 }
 
+static uint8_t
+convert_ucode_ltf_to_vendor_ltf(uint8_t ltf_type)
+{
+	switch (ltf_type) {
+	case 0:
+		return QCA_WLAN_VENDOR_CFR_LTF_TYPE_LEGACY;
+	case 1:
+		return QCA_WLAN_VENDOR_CFR_LTF_TYPE_HT;
+	case 2:
+		return QCA_WLAN_VENDOR_CFR_LTF_TYPE_VHT;
+	case 3:
+		return QCA_WLAN_VENDOR_CFR_LTF_TYPE_HE;
+	case 4:
+		return QCA_WLAN_VENDOR_CFR_LTF_TYPE_EHT;
+	default:
+		hdd_err("Invalid ltf type");
+		return QCA_WLAN_VENDOR_CFR_LTF_TYPE_LEGACY;
+	}
+}
+
 static int
 hdd_cfr_nl_put_phy_info(struct sk_buff *vendor_event,
 			struct cfr_enhanced_event_data *event_data)
@@ -302,6 +322,8 @@ hdd_cfr_nl_put_phy_info(struct sk_buff *vendor_event,
 		return -EINVAL;
 	}
 
+	event_data->ltf_type =
+		convert_ucode_ltf_to_vendor_ltf(event_data->ltf_type);
 	if (nla_put_u8(vendor_event,
 		       QCA_WLAN_VENDOR_ATTR_PEER_CFR_CSI_LTF_TYPE,
 		       event_data->ltf_type)) {
