arter97-mirror/caf_qcacld-3.0

commit c1b72e3ebb00294a90879980f695dfda06e3495f

qcacld-3.0: Skip channel validation when start bit is already set
When setband is issued during the CAC phase, after CAC completes,
userspace attempts to update beacon but encounters an invalid channel
error due to band restriction applied during CAC (e.g., restricting
to 2.4 GHz band only), further error path handling clears
SOFTAP_INIT_DONE bit unexpectedly.

Additionally, for MLO SAP, a race condition occurs with
hdd_softap_register_sta() which is called upon receiving
eSAP_START_BSS_EVENT. This function depends on mlo_vdev flag
checking to assign the correct link address. If the mlo_vdev flag
is incorrectly cleared, the adapter address is provided which leads
to cdp_peer_register() failure, and causing the vdev to go down
unexpectedly.

Since wlan_hdd_cfg80211_start_bss() could be called to just update
some IEs, and CSA will be triggered in another thread
sta_ap_intf_check_work in above mentioned case, check whether start
bit is already set and if yes, continue IEs update.

Change-Id: I558b17175e950a770cc161b9d015e9c23126a115
CRs-Fixed: 4386587

core/hdd/src/wlan_hdd_hostapd.c
@@ -7661,7 +7661,8 @@ int wlan_hdd_cfg80211_start_bss(struct wlan_hdd_link_info *link_info,
 		hdd_ctx->dev_dfs_cac_status = DFS_CAC_NEVER_DONE;
 
 	if (QDF_STATUS_SUCCESS !=
-	    wlan_hdd_validate_operation_channel(hdd_ctx, config->chan_freq)) {
+	    wlan_hdd_validate_operation_channel(hdd_ctx, config->chan_freq) &&
+	    !qdf_atomic_test_bit(SOFTAP_BSS_STARTED, link_info->link_flags)) {
 		hdd_err("Invalid Ch_freq: %d", config->chan_freq);
 		ret = -EINVAL;
 		goto error;

commit abd1182517ca7d38e891498bd4e567b67d2ed0be

qcacld-3.0: Fix a compiling issue
Fix a compiling issue "error: incompatible integer to pointer conversion
passing 'unsigned long' to parameter of type 'volatile unsigned long".

Fixes: c1b72e3ebb002 ("qcacld-3.0: Skip channel validation when start
bit is already set"
CRs-Fixed: 4509909
Change-Id: I2f07eed7afa8f98689ca77974f8b66a503eda473

core/hdd/src/wlan_hdd_hostapd.c
@@ -7676,7 +7676,7 @@ int wlan_hdd_cfg80211_start_bss(struct wlan_hdd_link_info *link_info,
 
 	if (QDF_STATUS_SUCCESS !=
 	    wlan_hdd_validate_operation_channel(hdd_ctx, config->chan_freq) &&
-	    !qdf_atomic_test_bit(SOFTAP_BSS_STARTED, link_info->link_flags)) {
+	    !test_bit(SOFTAP_BSS_STARTED, &link_info->link_flags)) {
 		hdd_err("Invalid Ch_freq: %d", config->chan_freq);
 		ret = -EINVAL;
 		goto error;