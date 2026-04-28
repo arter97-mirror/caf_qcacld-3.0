2ef5cda3c1b7dcdd67d8c0e3f120c36c18e1cf17
Jyoti Kumari <jyotkuma@qti.qualcomm.com>
2026-04-28 12:02:26 +0530

    qcacld-3.0: Fix UAF by nullifying limAssocResponseData after free
    Fix use-after-free in lim_process_assoc_rsp_frame() by setting
    session_entry->limAssocResponseData to NULL after freeing memory
    in error path. This prevents dangling pointer access on
    subsequent association attempts with the same session.
    
    The vulnerability can be triggered by malicious association
    response with invalid IE fields, causing
    lim_update_sta_vdev_punc() to fail and jump to error path where
    memory is freed but pointer remains.
    
    Change-Id: Id84cead3e7fd7691454380a2db0fcf2c4bc116aa
    CRs-Fixed: 4516383

--- core/mac/src/pe/lim/lim_process_assoc_rsp_frame.c
+++ core/mac/src/pe/lim/lim_process_assoc_rsp_frame.c
@@ -1852,6 +1852,8 @@ assocReject:
 free_mem:
 	qdf_mem_free(assoc_rsp->sha384_ft_subelem.gtk);
 	qdf_mem_free(assoc_rsp->sha384_ft_subelem.igtk);
+	if (session_entry->limAssocResponseData == assoc_rsp)
+		session_entry->limAssocResponseData = NULL;
 	qdf_mem_free(assoc_rsp);
 
 	return status;
