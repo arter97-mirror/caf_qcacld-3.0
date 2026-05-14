5c02ba2a0786a08c4ae384baa9cc77285a4a0603
Yu Ouyang <yuo@qti.qualcomm.com>
2026-05-14 15:11:29 +0800

    qcacld-3.0: Add 4th interface 5 GHz p2p GO support in auto pcl table
    Allow 4 interfaces from SAP(5 GHz) + STA(5 GHz) + SAP(2 GHz)SCC to
    P2P-GO(5 GHz) + SAP(5 GHz) + STA(5 GHz) + SAP(2 GHz) concurrency.
    
    Change-Id: Icea37f7842a4146727e2cd0bfc4f2d8d1a2aa545
    CRs-Fixed: 4536145

--- components/cmn_services/policy_mgr/src/wlan_policy_mgr_tables_2x2_dbs_i.h
+++ components/cmn_services/policy_mgr/src/wlan_policy_mgr_tables_2x2_dbs_i.h
@@ -5303,7 +5303,8 @@ fourth_connection_pcl_dbs_sbs_table
 	[PM_SAP_MODE] = { PM_5G, PM_5G, PM_5G } },
 	[PM_STA_SAP_SCC_5_SAP_24_DBS] = {
 	[PM_STA_MODE] = { PM_24G, PM_24G, PM_24G },
-	[PM_SAP_MODE] = { PM_5G, PM_5G, PM_5G } },
+	[PM_SAP_MODE] = { PM_5G, PM_5G, PM_5G },
+	[PM_P2P_GO_MODE] = { PM_5G, PM_5G, PM_5G } },
 	[PM_NAN_DISC_NDI_24_STA_5_DBS] = {
 	[PM_NDI_MODE] = { PM_NONE, PM_NONE, PM_NONE },
 	[PM_SAP_MODE] = { PM_SCC_ON_5_5G_24G, PM_SCC_ON_5_5G_24G,
