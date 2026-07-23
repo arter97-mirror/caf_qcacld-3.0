3436aa36d9e8372768dedb7f20aa4f0e96b82af4
Ravindra Konda <konda@qti.qualcomm.com>
2026-07-23 15:24:46 -0700

    Release 5.2.1.172
    Release 5.2.1.172
    
    Change-Id: Ibae37b4f6d65fa0195c0f50db4b01268819f0541
    CRs-Fixed: 4370095

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
 #define QWLAN_VERSION_PATCH            1
-#define QWLAN_VERSION_EXTRA            "Z"
-#define QWLAN_VERSION_BUILD            171
+#define QWLAN_VERSION_EXTRA            ""
+#define QWLAN_VERSION_BUILD            172
 
-#define QWLAN_VERSIONSTR               "5.2.1.171Z"
+#define QWLAN_VERSIONSTR               "5.2.1.172"
 
 #endif /* QWLAN_VERSION_H */
