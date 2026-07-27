6c4e7e7a38fef7dcbd2ca4ca6e8f1aa136372501
Naresh Pirati <npirati@qti.qualcomm.com>
2026-07-27 11:10:14 +0530

    qcacld-3.0: Assign wiphy to pdev osif_priv before pdev create
    Currently when host driver creates pdev, it invoke regulatory
    pdev created notification, from this notification, regulatory
    module schedules the registered callbacks. Since wiphy is
    assigned to pdev osif priv after pdev create, regulatory
    is not scheduing these callbacks as in hdd_dyn_cbk it needs to
    access wiphy from pdev osif priv.
    During idle restart, host drivers sets reguatory update in progress
    as true and this gets reset in hdd_dyn_cbk handling. Since reguatory
    is not calling this callback because of the above mentioned reason,
    this is not getting reset. Now whenever any operation comes and
    check for this flag, it waits for this event to complete which is
    never going to happen and eventually leads to timeout.
    To address above issue, assign wiphy to pdev osif priv before pdev
    create so that when pdev osif priv gets assigned to pdev it has
    updated wiphy info and that can be used in hdd_dyn_cbk. Along with
    it, invoke the registered reguatory callbacks from regulatory module
    so that regulatory update in progress can be reset.
    
    Change-Id: If20971f88b8ee894aa1f63cc8879bc98e6491f2c
    CRs-Fixed: 3978388
    (cherry picked from commit 55fc1a0ce798ef14da5938fd47a1cee206e9dd7e)

--- core/hdd/src/wlan_hdd_object_manager.c
+++ core/hdd/src/wlan_hdd_object_manager.c
@@ -43,8 +43,6 @@ static void hdd_init_pdev_os_priv(struct hdd_context *hdd_ctx,
 	struct pdev_osif_priv *os_priv)
 {
 	/* Initialize the OS private structure*/
-	os_priv->wiphy = hdd_ctx->wiphy;
-	os_priv->legacy_osif_priv = hdd_ctx;
 	wlan_cfg80211_scan_priv_init(hdd_ctx->pdev);
 	os_if_spectral_netlink_init(hdd_ctx->pdev);
 }
@@ -158,6 +156,9 @@ int hdd_objmgr_create_and_store_pdev(struct hdd_context *hdd_ctx)
 		hdd_debug("disabling 6ghz channels");
 		reg_cap_ptr->high_5ghz_chan = HIGH_5GHZ_FREQ_NO_6GHZ;
 	}
+
+	priv->wiphy = hdd_ctx->wiphy;
+	priv->legacy_osif_priv = hdd_ctx;
 
 	pdev = wlan_objmgr_pdev_obj_create(psoc, priv);
 	if (!pdev) {
